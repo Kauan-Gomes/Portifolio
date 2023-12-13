3d3668e72cf6810fd5c3ca966e3c5ce564acfe0e
Kauan Gomes <Kauang13@gmail.com>
2023-12-13 01:12:03 -0300

    arrumando o Home da landing page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Typed from 'typed.js';
 import React, { useEffect } from 'react'
+import Image from 'next/image';
 
 export default function Home() {
   useEffect(() => {
@@ -26,39 +27,53 @@ export default function Home() {
 
 
   return (
-    <div className='h-[90vh] mt-10 flex 2xl: lg: md: sm:'>
-      <div className='text-white flex flex-col items-center'>
-        <h1 className='mx-20 h-40 text-4xl font-bold' id='typed-output'></h1>
-        <div className='w-4/6 '>
-          <p className='text-xl my-5'>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-          <button className='border px-5 py-2 rounded-lg'>
-            Curriculo
-          </button>
+    <div className='
+    h-[90vh] my-10  flex flex-col items-center 
+    sm:flex-row sm:h-[35vh] sm:items-center sm:justify-center sm:p-5 
+    md:md:h-[35vh] md:mb-40 md:items-end
+    lg:h-[40vh] lg:text-center
+    xl:
+    '>
+      <div className='
+      text-white flex flex-col items-center 
+      sm:w-3/6 
+      md:w-4/6
+      
+      '>
+        <h1 className='
+        h-20 text-4xl text-center font-bold mx-2 py-0 w-full
+        sm:h-10
+        md:text-2xl 
+        lg:text-3xl
+        ' 
+        id='typed-output'></h1>
+        <div className='w-4/6 flex flex-col gap-5 lg:justify-center items-center'>
+          <p className='text-xl text-center my-5 sm:my-0 md:text-base'>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+          <div className='flex gap-5'>
+            <button className='relative font-bold px-5 py-2 rounded-lg'>
+              <a href="">Linkedin</a>
+              <span className='absolute inset-0 border-gradient-to-r pointer-events-none'></span>
+            </button>
+            <button className='relative text-white font-bold py-2 px-4 rounded-lg'>
+              <a href="">Github</a>
+              <span className='absolute inset-0 border-gradient-to-r pointer-events-none'></span>
+            </button>
+          </div>
         </div>
       </div>
-
-
-      {/* <div className='w-3/6 text-start flex flex-col -mr-80 gap-10 z-10 text-white xl: lg: md: sm:   '>
-        <h1 className='text-9xl text-Black font-bold xl:text-8xl'>Hello <br /> I'm Kauan Gomes</h1>
-        <div className='w-4/6 '>
-          <p className='text-xl my-5'>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-          <button className='border px-5 py-2 rounded-lg'>
-            Curriculo
-          </button>
+      <div className='relative flex items-center justify-center h-56 w-56 p-3 border-2 md:h-60 md:w-60  border-white rounded-full mt-20 '>
+          <div className='bg-red-300 h-full w-full rounded-full overflow-hidden'>
+            <Image
+              height={100}
+              width={100}
+              src='/fotoPerfil.jpg'
+              alt='Alternative text for the image'
+              layout='responsive'
+              sizes='(min-width: 640px) 30vw, 50vw' 
+              className='content object-cover'
+            />
+          </div>
         </div>
-      </div>
-      <div className='w-4/12 p-10'>
-        <Image
-          height={100}
-          width={100}
-          src='/fotoPerfil.jpg'
-          alt='Alternative text for the image'
-          layout='responsive'
-          sizes='(min-width: 640px) 30vw, 50vw'  // Ajuste o valor conforme necessário
-          className='rounded-lg'
-        />
-      </div> */}
-
     </div>
   )
 }
--- src/Components/Portifolio.tsx
+++ src/Components/Portifolio.tsx
@@ -1,36 +1,23 @@
-const Cards = ['Trabalho 1', 'Trabalho 2', 'Trabalho 3', 'Trabalho 4']
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+
+function Card(){
+    return(
+        <div className='bg-black h-96 w-96 rounded-lg'> 
+            
+        </div>
+    )
+} 
+
 
 export default function Portifolio() {
+
+    const Cards = ['Trabalho 1', 'Trabalho 2', 'Trabalho 3', 'Trabalho 4']
+
     return (
-        <div className='mt-20 flex flex-col justify-center items-center text-white h-screen '>
-            <h2 className='text-xl'>Portifolio</h2>
-            {Cards.map((item) => 
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        src="/fotoPerfil.jpg"
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {item}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>)}
-            
+        <div className='flex flex-col items-center text-white h-[80vh] gap-5'>
+            <h2 className='text-3xl'>Portifolio</h2>
+            {/* {Cards.map((item) => <Card/>)} */}
+        
         </div>
     )
 }
