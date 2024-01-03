6f676bd404be5b9e8bd2b41c0fe25964f8b073ab
Kauan Gomes <Kauang13@gmail.com>
2024-01-03 18:44:05 -0300

    animação de hover botão

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -10,20 +10,20 @@ export default function Home() {
 
   useLayoutEffect(() => {
     gsap.fromTo(".home",
-    {
-      x:0,
-      y:-70,
-      opacity: 0,
-      duration: 1,
-      rotate: "0deg",
-    },
-    {
-      x: 0,
-      y:0,
-      opacity: 1,
-      duration: 1,
-      rotate: "0deg",
-    })
+      {
+        x: 0,
+        y: 270,
+        opacity: 0,
+        duration: 1,
+        rotate: "0deg",
+      },
+      {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        rotate: "0deg",
+      })
 
     return () => {
       gsap.killTweensOf(".home")
@@ -53,17 +53,37 @@ export default function Home() {
   }, []);
 
 
+  const HandleMouseEnter = (e: any) => {
+    console.log(e)
+    gsap.to(e.querySelector('.Selecionado'), {
+      background: 'linear-gradient(to right, #09ff4626, #189ddb)',
+      duration: 0.5, // ajuste a duração conforme necessário
+      ease: 'power2.inOut',
+    });
+  }
+
+  const handleMouseLeave = (e: any) => {
+    gsap.to(e.querySelector('.Selecionado'), {
+      background: 'linear-gradient(to right, #000, #000)',
+      duration: 0.3, // ajuste a duração conforme necessário
+      ease: 'power2.inOut',
+    })
+  }
+
   return (
     <>
-      <div className=' text-white h-screen flex justify-center px-7 home opacity-0'>
-        <div className='flex flex-col justify-center gap-7'>
+      <div className=' text-white h-screen flex justify-center px-7 home opacity-0 '>
+        <div className='flex flex-col justify-center gap-7 md:w-8/12'>
           <h1 className='titulo text-5xl text-center font-semibold h-28'></h1>
-          <div className=' flex flex-col justify-center items-center gap-5 text-center'>
-            <p className=''>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+          <div className='flex flex-col justify-center items-center gap-5 text-center'>
+            <p className='text-bold'>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown</p>
             <div className='flex gap-5'>
-              <button className='relative h-14 w-auto flex gap-3 items-center p-[1px] font-bold rounded-lg cursor-pointer'>
+              <button
+                onMouseEnter={(e) => HandleMouseEnter(e.currentTarget)}
+                onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
+                className='relative h-14 w-auto flex gap-3 items-center p-[1px] font-bold rounded-lg cursor-pointer'>
                 <div className='absolute bubble-background2 h-full w-full left-0 rounded-lg z-0'></div>
-                <div className=' flex justify-center items-center h-full w-36 bg-black z-10 rounded-lg gap-4'>
+                <div className='flex justify-center items-center h-full w-36 bg-black z-10 rounded-lg gap-4 Selecionado'>
                   <div className='h-7 w-7'>
                     <Image
                       height={10}
@@ -80,9 +100,12 @@ export default function Home() {
                 </div>
 
               </button>
-              <button className='relative h-14 w-auto flex gap-3 items-center p-[1px] font-bold rounded-lg cursor-pointer'>
+              <button
+                onMouseEnter={(e) => HandleMouseEnter(e.currentTarget)}
+                onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
+                className='relative h-14 w-auto flex gap-3 items-center p-[1px] font-bold rounded-lg cursor-pointer hover'>
                 <div className='absolute bubble-background2 h-full w-full left-0 rounded-lg z-0'></div>
-                <div className=' flex justify-center items-center h-full w-36 bg-black z-10 rounded-lg gap-4'>
+                <div className=' flex justify-center items-center h-full w-36 bg-black z-10 rounded-lg gap-4 Selecionado'>
                   <div className='h-7 w-7'>
                     <Image
                       height={10}
@@ -97,16 +120,11 @@ export default function Home() {
                   </div>
                   <a className='' href="">Github</a>
                 </div>
-
               </button>
             </div>
           </div>
         </div>
-
       </div>
-
-    
-
     </>
   )
 }
